225afbcc6cd757641550010396d0918ffc951c23
matko-CT <soric.matko@gmail.com>
2018-11-29 15:04:39 +0100

    dataset prepared

--- src/main/scala/Netflix.scala
+++ src/main/scala/Netflix.scala
@@ -4,7 +4,6 @@ import java.util
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types._
 
-
 object Netflix {
 
   def main(args: Array[String]): Unit = {
@@ -14,33 +13,42 @@ object Netflix {
       .appName("Netflix Recommendation System")
       .getOrCreate;
 
-    val schema = new StructType()
+    val fourElementsSchema = new StructType()
       .add(StructField("movieId", LongType, true))
       .add(StructField("userId", LongType, true))
       .add(StructField("rating", LongType, true))
       .add(StructField("date", DateType, true))
 
-
-    val loadingDF = spark.read
+    val fourElementsDF = spark.read
       .option("header", "true")
-      .schema(schema)
+      .schema(fourElementsSchema)
       .parquet("netflix-data-parquet")
 
-    println(loadingDF.count())
+//    println(fourElementsDF.count())
 
+    val movieIdTitleSchema = new StructType()
+      .add(StructField("movieId", LongType, true))
+      .add(StructField("year", LongType, true))
+      .add(StructField("title", StringType, true))
 
-    //    val loadingDF = spark.read
-    //      .format("csv")
-    //      //      .option("header", "false")
-    ////            .option("header", "true")
-    //            .option("mode", "DROPMALFORMED")
-    //            .option("dateFormat", "yyyy-MM-dd")
-    //      .option("delimiter", ",")
-    ////            .option("inferSchema","true")
-    //      .load("src/main/resources/test.csv")
-    //
-    //
-    //    loadingDF.show(20);
+    val movieIdTitles = spark.read
+      .option("header", "true")
+      .schema(movieIdTitleSchema)
+      .csv("netflix-data/movie_titles.txt")
+
+//    println(movieIdTitles.count())
+
+    val combinedTitleRatingUser = fourElementsDF
+      .join(movieIdTitles, usingColumn = "movieId")
+
+    import spark.implicits._
+
+    combinedTitleRatingUser
+//      .select(combinedTitleRatingUser.col("*"))
+      .select($"movieId", $"title").distinct()
+      .filter("year = 1979")
+      .show(3000, false)
+
 
   }
 
