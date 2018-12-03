11620e9aabd53e1e3122fbe6beead8009944db87
matko-CT <soric.matko@gmail.com>
2018-12-03 15:08:26 +0100

    CrossValidator added, running with smaller data set

--- src/main/scala/Netflix.scala
+++ src/main/scala/Netflix.scala
@@ -3,9 +3,11 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.concurrent.TimeUnit
 
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.ml.recommendation.ALS
-import org.apache.spark.ml.tuning.ParamGridBuilder
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
@@ -20,10 +22,12 @@ object Netflix {
     val spark: SparkSession = org.apache.spark.sql.SparkSession.builder
       .master("local")
       .config("spark.sql.warehouse.dir", "spark-warehouse")
+
       .appName("Netflix Recommendation System")
       .getOrCreate;
 
     spark.sparkContext.setLogLevel("WARN")
+
 
     val fourElementsSchema = new StructType()
       .add(StructField("movieId", LongType, true))
@@ -101,29 +105,19 @@ object Netflix {
       .setRank(10)
       .setRegParam(0.01)
       .setAlpha(1.0)
-      .setMaxIter(5)
+      .setMaxIter(10)
       .setUserCol("userId")
       .setItemCol("movieId")
       .setRatingCol("rating")
       .setPredictionCol("prediction")
-      .fit(training)
-      .setColdStartStrategy("drop")
+//      .fit(training)
+//      .setColdStartStrategy("drop")
 
-    println("\n Model parameters explanations: \n" + model.explainParams)
-
-////    val als = new ALS()
-////      .setMaxIter(5)
-////      .setRegParam(0.01)
-////      .setUserCol("userId")
-////      .setItemCol("movieId")
-////      .setRatingCol("rating")
-////    val model = als.fit(training)
-////    model.setColdStartStrategy("drop")
-
-    val predictions = model.transform(test)
-    predictions.show(30, false)
+    println("\nModel parameters explanations: \n" + model.explainParams)
 
 
+//    val predictions = model.transform(test)
+//    predictions.show(30, false)
 
     // evaluation
     val evaluatorRMSE = new RegressionEvaluator()
@@ -139,37 +133,63 @@ object Netflix {
       .setLabelCol("rating")
       .setPredictionCol("prediction")
 
+//    val rmse = evaluatorRMSE.evaluate(predictions)
+//    val mse = evaluatorMSE.evaluate(predictions)
+//    val r2 = evaluatorR2.evaluate(predictions)
+//    println(f"Root-mean-square error = $rmse%1.2f" + "                 Is larger better? " + evaluatorRMSE.isLargerBetter)
+//    println(f"Mean-square error = $mse%1.2f" + "                       Is larger better? " + evaluatorMSE.isLargerBetter)
+//    println(f"Unadjusted coefficient of determination = $r2%1.2f" + "  Is larger better? " + evaluatorR2.isLargerBetter)
 
 
-    val rmse = evaluatorRMSE.evaluate(predictions)
-    val mse = evaluatorMSE.evaluate(predictions)
-    val r2 = evaluatorR2.evaluate(predictions)
+    // k-fold validation
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(model.implicitPrefs, Array(true, false))
+      .addGrid(model.rank, Array(3, 10, 20))
+      .addGrid(model.regParam, Array(0.001, 0.01, 0.1, 1))
+      .addGrid(model.alpha, Array(0.5, 1.0, 2.0))
+      .addGrid(model.maxIter, Array(5, 10, 20))
+      .addGrid(model.nonnegative, Array(true, false))
+      .build()
 
-    println(f"Root-mean-square error = $rmse%1.2f" + ". Is larger better? " + evaluatorRMSE.isLargerBetter)
-    println(f"Mean-square error = $mse%1.2f" + ". Is larger better? " + evaluatorMSE.isLargerBetter)
-    println(f"Unadjusted coefficient of determination = $r2%1.2f" + ". Is larger better? " + evaluatorR2.isLargerBetter)
 
+    val pipeline = new Pipeline()
+      .setStages(Array(model))
+
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluatorRMSE)
+//      .setEvaluator(evaluatorR2)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(10)
+//      .setParallelism(2)
+
+    cv.fit(training).avgMetrics
+
+    val best_als = cv.fit(training).bestModel
+
+
+
+
+    // concrete predictions
 
     // SLOW !!!
 //    val userRecs = model.recommendForAllUsers(10)
 //    val movieRecs = model.recommendForAllItems(10)
-
-
     // Generate top 10 movie recommendations for a specified set of users
-    val users = combinedTitleRatingUser.select(model.getUserCol).distinct().limit(3)
-    val userSubsetRecs = model.recommendForUserSubset(users, 10)
-    // Generate top 10 user recommendations for a specified set of movies
-    val movies = combinedTitleRatingUser.select(model.getItemCol).distinct().limit(3)
-    val movieSubSetRecs = model.recommendForItemSubset(movies, 10)
-
-    users.show(false)
-    userSubsetRecs.show(false)
-    movies.show(false)
-    movieSubSetRecs.show(false)
+//    val users = combinedTitleRatingUser.select(model.getUserCol).distinct().limit(3)
+//    val userSubsetRecs = model.recommendForUserSubset(users, 10)
+//    // Generate top 10 user recommendations for a specified set of movies
+//    val movies = combinedTitleRatingUser.select(model.getItemCol).distinct().limit(3)
+//    val movieSubSetRecs = model.recommendForItemSubset(movies, 10)
+//
+//    users.show(false)
+//    userSubsetRecs.show(false)
+//    movies.show(false)
+//    movieSubSetRecs.show(false)
 
 
     // running time
-    val minuteFormat = new SimpleDateFormat("mm")
+//    val minuteFormat = new SimpleDateFormat("mm")
     println("Start time: " + startHumanReadable)
     val endNano = System.nanoTime()
     val endHumanReadable = Calendar.getInstance().getTime()
@@ -182,9 +202,5 @@ object Netflix {
   }
 
 }
-
-
-
-//System.gc()
 
 //val zippedData = data.rdd.zipWithIndex()collect()
