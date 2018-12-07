2a563c8d829f394f53310d4dda9c20e85815ece3
matko-CT <soric.matko@gmail.com>
2018-12-07 14:17:31 +0100

    training script cleaned up

--- src/main/scala/Training.scala
+++ src/main/scala/Training.scala
@@ -5,12 +5,11 @@ import java.util.concurrent.TimeUnit
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.recommendation.{ALS, ALSModel}
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
-import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
 
-object Netflix {
+object Training {
 
   def main(args: Array[String]): Unit = {
 
@@ -76,65 +75,33 @@ object Netflix {
 
 
 
-
     // evaluation
 
     val evaluatorRMSE = new RegressionEvaluator()
       .setMetricName("rmse")
       .setLabelCol("rating")
       .setPredictionCol("prediction")
-//    val evaluatorMSE = new RegressionEvaluator()
-//      .setMetricName("mse")
-//      .setLabelCol("rating")
-//      .setPredictionCol("prediction")
-//    val evaluatorR2 = new RegressionEvaluator()
-//      .setMetricName("r2")
-//      .setLabelCol("rating")
-//      .setPredictionCol("prediction")
-//
-//    val rmse = evaluatorRMSE.evaluate(predictions)
-//    val mse = evaluatorMSE.evaluate(predictions)
-//    val r2 = evaluatorR2.evaluate(predictions)
-//    println(f"Root-mean-square error = $rmse%1.10f" + "                 Is larger better? " + evaluatorRMSE.isLargerBetter)
-//    println(f"Mean-square error = $mse%1.10f" + "                       Is larger better? " + evaluatorMSE.isLargerBetter)
-//    println(f"Unadjusted coefficient of determination = $r2%1.10f" + "  Is larger better? " + evaluatorR2.isLargerBetter)
-
 
     // k-fold validation
     val paramGrid = new ParamGridBuilder()
       .addGrid(alsModel.implicitPrefs, Array(true, false))
       .addGrid(alsModel.rank, Array(3, 4))
-      .addGrid(alsModel.alpha, Array(0.6, 1.0, 2.0))
-      .addGrid(alsModel.maxIter, Array(10, 15, 20))
+      .addGrid(alsModel.alpha, Array(0.2, 0.4, 0.6, 1.0))
+      .addGrid(alsModel.maxIter, Array(5, 7, 10, 15))
       .build()
-
-
-//    val pipeline = new Pipeline()
-//      .setStages(Array(model))
 
     val cv = new CrossValidator()
       .setEstimator(alsModel)
       .setEvaluator(evaluatorRMSE)
       .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(3)
-//      .setParallelism(2)
-
+      .setNumFolds(4)
 
     val best_als : ALSModel = cv.fit(training).bestModel.asInstanceOf[ALSModel]
 
-
-    best_als.write.overwrite().save("exporting_model_ALS_6_12" + System.nanoTime())
-    best_als.save("exporting_model_ALS_backup_6_12" + System.nanoTime())
-
+    best_als.write.overwrite().save("exporting_model_ALS" + System.nanoTime())
+    best_als.save("exporting_model_ALS_backup" + System.nanoTime())
 
     println(best_als.parent.extractParamMap())
-
-
-
-
-
-
-
 
 
     // time metrics
